2c05efe5b3a6424b728e67459f279641831d80a4
BuildTools <unconfigured@null.spigotmc.org>
2018-10-18 18:46:34 +0200

    -Complete fix of the master

---
+++ HotelSimulationSE5/HotelSimulationSE5/MainForm.cs
@@ -0,0 +1,142 @@
+﻿using HotelEvents;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelSimulationSE5
+{
+    public partial class MainForm : Form
+    {
+        public int _refreshrateinterval = 500; 
+        private Timer _refresh_timer= new Timer();
+        bool started = false;
+        Eventadapter events = new Eventadapter();        
+
+        private Building _myHotel;
+        public MainForm()
+        {
+            InitializeComponent();
+            GenerateHotel();
+            GuestButton.Top = _myHotel.maxYcoordinate * _myHotel.segmentSizeY + _myHotel.segmentSizeY;
+            StopButton.Top = _myHotel.maxYcoordinate * _myHotel.segmentSizeY + _myHotel.segmentSizeY;
+            EventButton.Top = _myHotel.maxYcoordinate * _myHotel.segmentSizeY + _myHotel.segmentSizeY;
+
+            _refresh_timer.Interval = _refreshrateinterval;
+            _refresh_timer.Tick += _refresh_timer_Tick;
+            _refresh_timer.Start();
+            Console.WriteLine();
+
+        }
+
+        private void _refresh_timer_Tick(object sender, EventArgs e)
+        {
+            if (events.EventList.Count()>0)
+            {
+                foreach (var item in events.EventList)
+                {
+                    switch (item.EventType)
+                    {
+                        case HotelEventType.CHECK_IN:
+                            _myHotel.Create_Guest(_myHotel.elevatorNodes[_myHotel.maxYcoordinate-1]);
+                            break;
+                        case HotelEventType.CHECK_OUT:
+
+                            break;
+                        case HotelEventType.CLEANING_EMERGENCY:
+
+                            break;
+                        case HotelEventType.EVACUATE:
+
+                            break;
+                        case HotelEventType.GODZILLA:
+
+                            break;
+                        case HotelEventType.GOTO_CINEMA:
+
+                            break;
+                        case HotelEventType.GOTO_FITNESS:
+
+                            break;
+                        case HotelEventType.NEED_FOOD:
+
+                            break;
+                        case HotelEventType.NONE:
+
+                            break;
+                        case HotelEventType.START_CINEMA:
+
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+                events.EventList.Clear();
+            }
+
+
+            //Move guests
+            _myHotel.Move_Guest(this);
+        }
+
+        public void GenerateHotel()
+        {
+            _myHotel = new Building();
+            _myHotel.CreateHotel(this);
+        }
+
+        private void GuestButton_Click(object sender, EventArgs e)
+        {
+            _myHotel.Create_Guest(_myHotel.elevatorNodes[_myHotel.maxYcoordinate-1]);
+            _refresh_timer.Start();
+        }
+
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            _refresh_timer.Stop();
+        }
+
+
+        private void EventButton_Click(object sender, EventArgs e)
+        {
+            if (started == false)
+            {
+                events.Register(events);
+                events.Start_events();
+                started = true;
+            }
+            else
+            {
+                events.Stop_Events();
+                started = false;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _myHotel.ReloadAvailableRooms();
+            foreach (Guest arrival in _myHotel._guestList)
+            {
+                if (_myHotel.AvailableRooms.Count() > 0)
+                {
+                    arrival.MyRoom = _myHotel.AssignRoom(_myHotel.AvailableRooms[0].ID);
+                    arrival.MyRoom.Reserved = true;
+                    arrival.Path = arrival.MyNode.Pathfinding(arrival.MyNode, arrival.MyRoom);
+                }
+            }
+        }
+    }
+}
