96da2101bbee7cb90e74b3c34625951ddbecb1f1
ShiAkl <shivam_akloe@hotmail.com>
2018-10-21 14:43:22 +0200

    Building Class failing tests

--- HotelSimulationSE5/UnitTestProject1/UnitTest1.cs
+++ HotelSimulationSE5/UnitTestProject1/UnitTest1.cs
@@ -1,20 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelSimulationSE5;
+using HotelSimulationSE5.HotelSegments;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        #region Building.cs
         [TestMethod]
-        public void TestMethod1()
+        public void AssignRoomWithExistingID()
         {
             // Arrange
-
+            var MySegment = new GuestRoom();
+            var value = 3;
             // Act
-
+            GuestRoom test = Building.AssignRoom(value);
             // Asset
 
         }
+
+        [TestMethod]
+        public void AssignRoomWithoutExistingID()
+        {
+            // Arrange
+            var MySegment = new GuestRoom();
+            var value = 3;
+            // Act
+            GuestRoom test = Building.AssignRoom(value);
+            // Asset
+
+        }
+
+        [TestMethod]
+        public void ReloadAvailableRooms()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Asset
+
+        }
+        #endregion
+        #region Node.cs
+
+        #endregion
     }
 }
+
